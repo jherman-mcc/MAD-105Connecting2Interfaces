af874b57e979d8797a963458c24ddd649e3f8698
JH <https://github.com/jherman-mcc/MAD-105aboutme.git>
2021-07-12 22:01:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/connecting2interfaces/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.connecting2interfaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+open class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
